57392d79054727097d8d53eae53505bd99e9eb9a
Valentin Kiel <valentin.kil.2002@gmail.com>
2022-08-30 14:26:55 +0300

    hcl2yml in client/config.go

--- client/config.go
+++ client/config.go
@@ -2,18 +2,18 @@ package client
 
 // Config defines Provider Configuration
 type Config struct {
-	OrganizationIDs []string `hcl:"organization_ids,optional"`
-	CloudIDs        []string `hcl:"cloud_ids,optional"`
-	FolderIDs       []string `hcl:"folder_ids,optional"`
+	OrganizationIDs []string `yml:"organization_ids,optional"`
+	CloudIDs        []string `yml:"cloud_ids,optional"`
+	FolderIDs       []string `yml:"folder_ids,optional"`
 
-	FolderFilter string `hcl:"folder_filter,optional"`
+	FolderFilter string `yml:"folder_filter,optional"`
 }
 
 func (c Config) Example() string {
-	return `configuration {
-				// Optional. Filter as described https://cloud.yandex.com/docs/resource-manager/grpc/folder_service#List
-				// folder_filter = ""
-				// Optional. If not specified either using all folders accessible.
-				// folder_ids = [<CHANGE_THIS_TO_YOUR_FOLDER_ID>]
-			}`
+	return `---
+# Optional. Filter as described https://cloud.yandex.com/docs/resource-manager/grpc/folder_service#List
+folder_filter: ""
+# Optional. If not specified either using all folders accessible.
+folder_ids: [<CHANGE_THIS_TO_YOUR_FOLDER_ID>]
+`
 }
