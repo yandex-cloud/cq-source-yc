6db86ff3a9ee11a269d7d51bca4294db4e7bddc1
Валентин Киль <valentin.kil.2002@gmail.com>
2022-09-07 14:25:55 +0300

    merge main to update-sdk-v0.14.7+

--- client/config.go
+++ client/config.go
@@ -6,18 +6,24 @@ type Config struct {
 	CloudIDs        []string `yaml:"cloud_ids"`
 	FolderIDs       []string `yaml:"folder_ids"`
 
+	Endpoint     string `yaml:"endpoint"`
 	FolderFilter string `yaml:"folder_filter"`
 }
 
 func (c Config) Example() string {
-	return `---
-Optional. If not specified either using all clouds accessible.
-cloud_ids:
+	return `
+# Specify either organization_ids, cloud_ids or folder_ids
+# Optional
+# organization_ids:
+# Optional 
+# cloud_ids:
+# Optional. If not specified either using all folders accessible.
+# folder_ids:
 
-Optional. If not specified either using all folders accessible. Might not work without folder specified
-folder_ids:
+# Optional.
+# endpoint: api.cloud.yandex.net:443
 
-Optional. Filter as described https://github.com/yandex-cloud/cloudapi/blob/master/yandex/cloud/resourcemanager/v1/folder_service.proto
-folder_filter: 
+# Optional. Filter as described https:#cloud.yandex.com/docs/resource-manager/grpc/folder_service#List
+# folder_filter:
 `
 }
