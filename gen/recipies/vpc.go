467ef56e542615baca9388b7ca16a223e2846b4c
candiduslynx <candiduslynx@gmail.com>
2022-09-23 17:03:30 +0300

    vpc

--- gen/recipies/vpc.go
+++ gen/recipies/vpc.go
@@ -1,5 +1,43 @@
 package recipies
 
+import (
+	"github.com/cloudquery/plugin-sdk/codegen"
+	"github.com/yandex-cloud/go-genproto/yandex/cloud/vpc/v1"
+)
+
 func VPC() []*Resource {
-	return []*Resource{}
+	return []*Resource{
+		{
+			Service:      "vpc",
+			SubService:   "addresses",
+			Struct:       new(vpc.Address),
+			SkipFields:   []string{id},
+			ExtraColumns: codegen.ColumnDefinitions{idCol},
+			Multiplex:    multiplexFolder,
+		},
+		{
+			Service:      "vpc",
+			SubService:   "networks",
+			Struct:       new(vpc.Network),
+			SkipFields:   []string{id},
+			ExtraColumns: codegen.ColumnDefinitions{idCol},
+			Multiplex:    multiplexFolder,
+		},
+		{
+			Service:      "vpc",
+			SubService:   "security_groups",
+			Struct:       new(vpc.SecurityGroup),
+			SkipFields:   []string{id},
+			ExtraColumns: codegen.ColumnDefinitions{idCol},
+			Multiplex:    multiplexFolder,
+		},
+		{
+			Service:      "vpc",
+			SubService:   "subnets",
+			Struct:       new(vpc.SecurityGroup),
+			SkipFields:   []string{id},
+			ExtraColumns: codegen.ColumnDefinitions{idCol},
+			Multiplex:    multiplexFolder,
+		},
+	}
 }
---
+++ resources/services/vpc/security_groups_fetch.go
@@ -0,0 +1,20 @@
+package vpc
+
+import (
+	"context"
+
+	"github.com/cloudquery/plugin-sdk/schema"
+	"github.com/yandex-cloud/cq-provider-yandex/client"
+	"github.com/yandex-cloud/go-genproto/yandex/cloud/vpc/v1"
+)
+
+func fetchSecurityGroups(ctx context.Context, meta schema.ClientMeta, _ *schema.Resource, res chan<- interface{}) error {
+	c := meta.(*client.Client)
+
+	it := c.Services.VPC.SecurityGroup().SecurityGroupIterator(ctx, &vpc.ListSecurityGroupsRequest{FolderId: c.MultiplexedResourceId})
+	for it.Next() {
+		res <- it.Value()
+	}
+
+	return nil
+}
